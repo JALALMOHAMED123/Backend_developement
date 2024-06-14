7e412d335e525d51da6e142dc9138e14debb8fdf
Jalal Mohamed <jalalmohamed2702@gmail.com>
2024-06-14 08:36:57 +0530

    Arrow & student object

---
+++ Arrow_Function.js
@@ -0,0 +1,12 @@
+const mul=(a,b)=> a*b;
+
+console.log(mul(1,2));
+
+const Student={
+    name: 'hari',
+    age: 25,
+    fun: function(){
+        console.log("Hi, My name is "+this.name);
+    }
+}
+Student.fun();
