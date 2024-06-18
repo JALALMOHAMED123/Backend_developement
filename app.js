22152e5dbf92557baaaecbc98f5186de7b435c29
Jalal Mohamed <jalalmohamed2702@gmail.com>
2024-06-18 23:44:32 +0530

    Nodemon & debugging

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-const http=require('http');
+const http = require('http');
 
-const server=http.createServer((req,res)=>{
-    // console.log("Jalal Mohamed");
-    // console.log(req);
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('context-type', 'text/html');
-    
+const routes = require('./routes');
 
-    if(req.url=='/home')
-        {
-            res.write('<body><h1>Welcome home</h1></body>');
-        }
-    else if(req.url=='/about')
-        {
-            res.write('<body><h1>Welcome to About Us page</h1></body>');
-        }
-    else if(req.url=='/node')
-        {
-            res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-        }
-    else{
-        res.write('<html>');
-        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-        res.write('</html>');
-    }
-});
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(4000);
